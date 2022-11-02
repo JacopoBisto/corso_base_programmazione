6c2da81302e012bcadb4247378a82ffeb841dae0
JacopoBisto <115649807+JacopoBisto@users.noreply.github.com>
2022-11-02 15:37:12 +0100

    Add files via upload

---
+++ max.min.while.py
@@ -0,0 +1,10 @@
+numero = int(input("inserisci un numero: "))
+max = numero
+min = numero
+while numero != 0: 
+    if(numero > max):
+        max = numero
+    if(numero < min):
+        min = numero
+    numero = int(input("inserisci un numero: "))
+print("Il numero massimo è ", max)
