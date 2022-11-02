0aefa3f16d582a8268bc9b8ba79e6ce89115cccf
JacopoBisto <115649807+JacopoBisto@users.noreply.github.com>
2022-11-02 15:33:34 +0100

    Add files via upload

---
+++ if.numeri.py
@@ -0,0 +1,11 @@
+n1 = int(input("inserisci il primo numero: "))
+n2 = int(input("inserisci il secondo numero: ")) 
+if (n1 > 10):
+    print("FUORI SCALA")
+else :
+    print("TUTTO OK")
+if(n2 > 50):
+    print("TROPPO GRANDE")
+else:
+    print("TUTTO OK")
+    
